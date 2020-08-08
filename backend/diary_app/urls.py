69e3a7881f42b0933b2599b08594f1cf864a1ecb
diegofigueroa79 <diegofigueroa79@gmail.com>
2020-08-08 15:13:27 -0700

    created listview and detailview for article api

--- backend/diary_app/urls.py
+++ backend/diary_app/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 
+from .views import ArticleListView, ArticleDetailView
 
 app_name = "diary"
 urlpatterns = [
+    path('', ArticleListView.as_view()),
+    path('', ArticleDetailView.as_view()),
 ]
--- backend/diary_app/views.py
+++ backend/diary_app/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from .models import Article
+from .serializers import ArticleSerializer
+from rest_framework.generics import ListAPIView, RetrieveAPIView
 # Create your views here.
+
+class ArticleListView(ListAPIView):
+    queryset = Article.objects.all()
+    serializer_class = ArticleSerializer
+
+class ArticleDetailView(RetrieveAPIView):
+    queryset = Article.objects.all()
+    serializer_class = ArticleSerializer
