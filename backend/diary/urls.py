90afc54a9459570ada608888df93823d5e68dbc8
diegofigueroa79 <diegofigueroa79@gmail.com>
2020-08-08 15:13:00 -0700

    fixed issue with missing trailing slash

--- backend/diary/urls.py
+++ backend/diary/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-	path('diary/api', include("diary_app.urls")),
+	path('diary/api/', include("diary_app.urls")),
 ]
